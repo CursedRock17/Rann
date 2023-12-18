bd431840a544f8429f357e2202f46e8296f8c17e
CursedRock17 <mtglucas1@gmail.com>
2023-12-17 23:31:46 -0500

    Adding a better loader
    Signed-off-by: CursedRock17 <mtglucas1@gmail.com>

--- frontend/utils/loader.py
+++ frontend/utils/loader.py
@@ -1,2 +1,28 @@
 import os
 import glob as glob
+import string_assistance as string_
+
+
+def findFiles(path):
+    return glob.glob(path)
+
+
+def splitCode(filename):
+    # Read the file and split the file into lines
+    lines = open('data/%s.txt' % (filename), encoding='utf-8').read().strip()
+    lines = (["def" + func for func in lines.split("def")])
+    return lines
+
+
+def splitLabels(filename):
+    lines = open('data/%s.txt' % (filename), encoding='utf-8').read().strip().split('\n')
+    lines = [[string_.normalizeString(string) for string in line.split('\t')] for line in lines]
+    return lines
+
+
+def organizePairs(code_filename, label_filename):
+    pairs = [splitCode(code_filename), splitLabels(label_filename)]
+    return pairs
+
+
+pairs = organizePairs("code", "codeLabels")
